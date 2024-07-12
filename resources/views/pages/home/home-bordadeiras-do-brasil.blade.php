f99cb26d27b360bf2aa04783a8d0ea95057e1a55
williambrito98 <williambrito199850@gmail.com>
2024-07-12 19:17:26 -0300

    fix: home posts

--- resources/views/pages/home/home-bordadeiras-do-brasil.blade.php
+++ resources/views/pages/home/home-bordadeiras-do-brasil.blade.php
@@ -12,26 +12,25 @@
                         <h4 class="post-headline">Sobre a Plataforma</h4>
                     </a>
                     <p align="justify">
-                        A plataforma Bordadeiras do Brasil faz parte do projeto A Arte de Bordar pelo Brasil, que foi
+                        A Plataforma Bordadeiras do Brasil faz parte do projeto A Arte de Bordar pelo Brasil, que foi
                         criado em 2019 pela artista Fernanda Nadal, designer de bordados multifacetada, que atua como
                         professora, empresária, mentora e escritora.
                         <br>
                         <br>
                         O projeto, hoje realizado através da Lei Federal de Incentivo à Cultura com o patrocínio de
-                        empresas incentivadoras, já contemplou mais de 400
-                        mulheres com formação na arte do bordado em pedraria.
+                        empresas incentivadoras, já contemplou mais de 400 mulheres com formação na arte do bordado em
+                        pedrarias, e já passou pelas cidades de Montenegro/RS, João Monlevade/MG, Campinas/SP,
+                        Indaiatuba/SP e Mogi Guaçu/SP.
                         <br>
                         <br>
-                        Em 2023, deu-se início à criação desta
-                        plataforma, Bordadeiras do Brasil, com a proposta de possibilitar a divulgação do trabalho das
-                        participantes dos cursos realizados por Fernanda que tenham a intenção de dar continuidade a
-                        trabalhos na área, junto à pessoas e empresas interessadas na mão de obra com o bordado em
-                        pedraria.
+                        EEm 2023, deu-se início à criação desta Plataforma, Bordadeiras do Brasil, com a proposta de
+                        possibilitar a divulgação do trabalho das participantes dos cursos realizados no projeto, que
+                        tenham a intenção de dar continuidade a trabalhos na área, junto à pessoas e empresas
+                        interessadas na mão de obra com o bordado em pedrarias.
                         <br>
                         <br>
-                        Esta plataforma representa um pilar fundamental no trabalho da artista Fernanda Nadal, que
-                        pretende oferecer oportunidades para que mulheres possam obter uma fonte de renda por meio do
-                        bordado em pedraria. Através desta plataforma, pretende-se que interessados em contratar
+                        Esta plataforma pretende oferecer oportunidades para que mulheres possam obter uma fonte de
+                        renda por meio do bordado em pedrarias. Através dela, pretende-se que interessados em contratar
                         serviços de bordado em pedrarias, seja individualmente ou como entidades corporativas, possam
                         encontrar profissionais qualificadas em suas regiões de interesse.
                     </p>
@@ -40,17 +39,16 @@
                     <p align="justify">
                         <strong>QUEM PODE PARTICIPAR:</strong>
                         <br>
-                        Esta plataforma é destinada a mulheres participantes do curso A Arte de
-                        Bordar, realizado por Fernanda. As inscrições são abertas conforme patrocínios realizados nas
-                        cidades, através de empresas que destinam parte do seu Imposto de Renda à cultura, via Lei de
-                        Incentivo.
+                        Esta plataforma é destinada a mulheres participantes do curso A Arte de Bordar pelo Brasil,
+                        realizado através da Lei Federal de Incentivo à Cultura. As inscrições são abertas conforme
+                        patrocínios realizados nas cidades, através de empresas que destinam parte do seu Imposto de
+                        Renda à cultura, via Lei de Incentivo.
                         <br>
                         <br>
-                        As participantes de cada edição do curso interessadas em dar continuidade no
-                        aprendizado adquirido durante a formação, poderão ter o seu material nesta plataforma. Durante o
-                        próprio curso, as mulheres receberão um tecido com um traço pré-determinado e entregue pela
-                        Fernanda, que deverá ser bordado com as pedrarias disponibilizadas pelo projeto, utilizando-se
-                        dos pontos ensinados em aula.
+                        As participantes de cada edição do curso interessadas em dar continuidade no aprendizado
+                        adquirido durante a formação, poderão ter o seu material nesta plataforma, de acordo com uma
+                        análise feita pela própria Fernanda junto à sua equipe, no intuito de divulgar os seus
+                        trabalhos, para que interessados possam entrar em contato diretamente com as alunas do projeto.
                         <br>
                         <br>
                         A própria Fernanda analisará o material recebido por cada aluna,
@@ -69,6 +67,6 @@
 
     @include('pages.components.link-destacado', [
     'url' => 'https://www.projetosestrategicos.com.br/contato.php',
-    'text' => 'Clique aqui para saber como investir neste projeto'
+    'text' => 'Saiba como levar este projeto para a sua cidade, clicando aqui'
     ])
 </section>
