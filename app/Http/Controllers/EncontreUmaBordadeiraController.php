7eb3af69cfd57f364fb82e4eb11d54fe7466ddc5
williambrito98 <williambrito199850@gmail.com>
2024-07-12 19:18:28 -0300

    fix: page description

--- app/Http/Controllers/EncontreUmaBordadeiraController.php
+++ app/Http/Controllers/EncontreUmaBordadeiraController.php
@@ -10,7 +10,7 @@ class EncontreUmaBordadeiraController extends Controller
     {
 
         $title = 'Encontre uma Bordadeira';
-        $description = '<p style="font-size: 16px" align="justify">Nessa aba você encontra o bordado realizado através de um traço pré-determinado, realizado pelas alunas que fizeram o curso a arte de bordar em pedrarias com a Fernanda Nadal e foram bem avaliadas por ela, bem como imagens de alguns outros trabalhos já desenvolvidos pela aluna, caso tenham. É possível acessar o contato e conversar diretamente com cada uma delas, clicando nas imagens abaixo.<br><br>Caso você tenha dúvidas sobre qual bordadeira contatar para a sua demanda, é possível contatar a própria Fernanda Nadal, através do seu email, disponível nesta plataforma.<br><br></p>';
+        $description = '<p style="font-size: 16px" align="justify">Na aba da Plataforma “Encontre uma bordadeira” a pessoa ou empresa interessada em contratar serviço de bordado em pedrarias poderá acessar as bordadeiras cadastradas através dos seus contatos e redes sociais, além de conhecer um pouco da história de cada uma.<br><br></p>';
 
         $bordadeiras = Bordadeiras::all();
 
