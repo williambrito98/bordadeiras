f9a319488fe2925bf087bfca84b61a7632769860
william <william.brito@kwinfo.com.br>
2025-03-24 10:07:53 -0300

    fix: layout dos patrocionadores

--- resources/views/pages/apoiadores/apoiadores-patrocinadores.blade.php
+++ resources/views/pages/apoiadores/apoiadores-patrocinadores.blade.php
@@ -1,11 +1,11 @@
 <x-title text="Patrocinadores"/>
 <div class="container">
-    <div class="row">
+    <div class="row" style="justify-content: center;align-items: center;">
         @foreach($patrocinadores as $patrocinador)
             <div class="col-6 col-md-4 col-lg-3" align="center">
                 <div class="single-post wow fadeInUp" data-wow-delay="0.1s"
                      style="visibility: visible; animation-delay: 0.1s; animation-name: fadeInUp;">
-                    <div class="post-thumb">
+                    <div class="">
                         <a href="{{$patrocinador['site_url']}}" target="_blank">
                             <img src="{{$patrocinador['logo_url']}}" style=" padding: 0; margin: 0; " alt="">
                         </a>
