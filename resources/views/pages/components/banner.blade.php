ea414266a9ba9e9dc173e2d3e1a3e88337439106
william <william.brito@kwinfo.com.br>
2025-03-24 10:08:03 -0300

    fix: banner url

--- resources/views/pages/components/banner.blade.php
+++ resources/views/pages/components/banner.blade.php
@@ -5,7 +5,7 @@
         <div class="col-12">
             <div class="footer-content">
                 <div class="footer-logo-area text-center">
-                    <img src="{{ $banner ? $banner->img_url : '/img/BARRA DE LOGOS.png' }}"
+                    <img src="{{ $banner && $banner->img_url ? $banner->img_url : '/img/BARRA DE LOGOS.png' }}"
                         alt="Barra com os logos dos patrocionadores, incentivadores e apoiadores" class="img-fluid"
                         style="height: 100%">
                 </div>
