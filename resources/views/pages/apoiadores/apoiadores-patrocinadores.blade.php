74bcad2019f6a768b0aaa750354b9787a1546d1b
william <william.brito@kwinfo.com.br>
2025-05-05 13:51:25 -0300

    fix: layout da sessao dos patrocinadores

--- resources/views/pages/apoiadores/apoiadores-patrocinadores.blade.php
+++ resources/views/pages/apoiadores/apoiadores-patrocinadores.blade.php
@@ -2,12 +2,12 @@
 <div class="container">
     <div class="row" style="justify-content: center;align-items: center;">
         @foreach($patrocinadores as $patrocinador)
-            <div class="col-6 col-md-4 col-lg-3" align="center">
+            <div class="col-12" align="center">
                 <div class="single-post wow fadeInUp" data-wow-delay="0.1s"
                      style="visibility: visible; animation-delay: 0.1s; animation-name: fadeInUp;">
                     <div class="">
                         <a href="{{$patrocinador['site_url']}}" target="_blank">
-                            <img src="{{$patrocinador['logo_url']}}" style=" padding: 0; margin: 0; width: 200px; height: 200px; object-fit: contain;" alt="">
+                            <img src="{{$patrocinador['logo_url']}}" style=" padding: 0; margin: 0; width: 100%; height: auto; object-fit: contain;" alt="">
                         </a>
                     </div>
                 </div>
